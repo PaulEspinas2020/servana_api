b75ce4755229cb9a4dd5cc16b020be1256e96bd2
Clarence Hautea <clarence@moveup.app>
2026-04-10 04:01:36 +0800

    update env

--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -7,7 +7,6 @@ const router = Router();
 router.post("/:bookingId/gcash-submit", paymentController.gcashSubmit);
 router.post("/:bookingId/approve", paymentController.approve);
 router.post("/:bookingId/mark-cash-paid", paymentController.markCashPaid);
-
 router.post("/:bookingId/paymongo/create", paymentController.createPaymongoPayment);
 router.post("/paymongo/webhook", paymentController.paymongoWebhook);
 
